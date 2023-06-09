6b3babd34e9c7bd8652d54035a498dbaf20685f4
akbarali-lutfulayev <akbaralilutfulayev@gmail.com>
2023-06-09 16:53:01 +0500

    hello bitch

---
+++ src/pages/akbarali2/akbar2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Akbar2() {
+    return (
+        <div className="wrapper2">
+            <div className="akbarbe18">
+                
+            </div>
+        </div>
+    )
+}
+
+
